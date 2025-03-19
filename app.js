541efb8f83fd62807af6ca2c24ed4c5eaf378de8
Cheftonic <161465516+cheftonic92@users.noreply.github.com>
2025-03-19 11:54:50 +0000

    Finalización del ejercicio

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 // we declare a new global variable containing an array that represents the ballons map
 // you have to add more colors into the ballonsMap array
-let ballonsMap = ['green'];
+let ballonsMap = ['red' ,'green', 'blue', 'pink', 'grey', 'purple', 'yellow', 'grey', 'pink', 'grey', 'black', ' green', 'brown', 'purple', 'blue', 'yellow', 'green', 'black', 'red', 'pink'];
 
 // poping a balloon is basically turning his color to null (no color)
 const popBalloon = (position) => {
     // set the color to null on the balloon position
+    ballonsMap[position] = null;
+
     render();
 }
 
@@ -12,14 +14,33 @@ const render = () => {
     
     // convert ballons map of colors into real html balloons
     const ballons = ballonsMap.map((color, position) => {
-        return `<div class="balloon active"></div>`; // <--- render each balloon
+        if (color !== null) {
+            // Globo activo con color
+            return `
+                <div 
+                  class="balloon active" 
+                  style="background-color: ${color};"
+                  onclick="popBalloon(${position})"
+                ></div>
+            `;
+        } else {
+            // Globo reventado
+            return `<div class="balloon popped"></div>`;
+        }
     });
 
-    document.querySelector("#balloon-count").innerHTML = ballons.filter(b => b !== null).length; // <-- render the balloon count into the DOM
-    document.querySelector("#balloon-map").innerHTML = ballons.join(''); // <-- render the balloons into the DOM
+    const activeBalloons = ballonsMap.filter(color => color !== null).length;
+    document.querySelector("#balloon-count").innerHTML = activeBalloons;
+    document.querySelector("#balloon-map").innerHTML = ballons.join("");
+  
+    // Si ya no hay globos activos, recargamos la página para reiniciar el juego.
+    if (activeBalloons === 0) {
+      setTimeout(() => {
+        window.location.reload();
+      }, 500); // Opcional: un pequeño retardo antes de recargar.
+    }
+  };
 
-    if(activeBalloons == 0) window.location.reload(); // <--- reload website when no more balloons are left
-}
 
 // this makes the "render" function trigger when the website starts existing
 window.onload = render();
